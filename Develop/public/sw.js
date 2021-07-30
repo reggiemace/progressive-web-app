67bbc19b6968f73496e839c49e1d5ac7235c80e4
Reeggie Mace <reginaldmace36@gmail.com>
2021-07-30 09:27:07 -0400

    added caching to service worker fetch function

--- Develop/public/sw.js
+++ Develop/public/sw.js
@@ -1,12 +1,50 @@
+const staticCacheName = "static-resources";
+const dynamicCacheName = "dynamic-resources-v1";
+const assets = [
+  "/",
+  "app.js",
+  "index.html",
+  "index.js",
+  "styles.css",
+  "manifest.json",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+];
 // install service worker
 self.addEventListener("install", (evt) => {
-  console.log("service worker has been installed");
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("caching shell assets");
+      cache.addAll(assets);
+    })
+  );
 });
 // activate service worker
 self.addEventListener("activate", (evt) => {
-  console.log("service worker has been activated");
+  evt.waitUntil(
+    caches.keys().then((keys) => {
+      //console.log(keys);
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCacheName)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
 });
 // fetch from server
 self.addEventListener("fetch", (evt) => {
-  console.log("service worker is fetching", evt);
+  //console.log("service worker is fetching", evt);
+  evt.respondWith(
+    caches.match(evt.request).then((cacheRes) => {
+      return (
+        cacheRes ||
+        fetch(evt.request).then((fetchRes) => {
+          return caches.open(dynamicCacheName).then((cache) => {
+            cache.put(evt.request.url, fetchRes.clone());
+            return fetchRes;
+          });
+        })
+      );
+    })
+  );
 });
